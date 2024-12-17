819604e22f240f06930752b3c082fb556d439c3a
Reynaldi Kindarto <37333204+3instein@users.noreply.github.com>
2024-12-17 16:25:05 +0700

    refactor: Simplify Home component layout by removing unnecessary flex properties

--- app/page.js
+++ app/page.js
@@ -2,8 +2,8 @@ import ChristmasOpening from "./components/ChristmasOpening";
 
 export default function Home() {
   return (
-    <div className="flex justify-center items-center h-screen">
-      <ChristmasOpening />
+    <div>
+      <ChristmasOpening/>
     </div>
   );
 }
