7e8e24dbbc26ee26f25816f717c27939cb3f48be
Reynaldi Kindarto <37333204+3instein@users.noreply.github.com>
2024-12-22 20:02:56 +0700

    feat: Add PhotoTree and ImageContainer components with styling and photo generation logic

--- app/page.js
+++ app/page.js
@@ -1,8 +1,10 @@
 import ChristmasOpening from "./components/ChristmasOpening";
+import PhotoTree from "./components/PhotoTree";
 
 export default function Home() {
   return (
-    <div>
+    <div className="p-4">
+      {/* <PhotoTree /> */}
       <ChristmasOpening/>
     </div>
   );
